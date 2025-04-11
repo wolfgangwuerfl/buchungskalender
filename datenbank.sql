3b597ad5cc1ab507421f3e70406b3c166ff14667
Wolfgang Würfl <info@it-wuerfl.de>
2025-04-11 09:59:22 +0200

    update sql

--- datenbank.sql
+++ datenbank.sql
@@ -1,9 +1,12 @@
--- Tabelle für Buchungen mit mehreren Räumen
-CREATE TABLE buchungen (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  datum DATE NOT NULL,
-  raum VARCHAR(50) NOT NULL,
-  name VARCHAR(100),
-  email VARCHAR(100),
-  UNIQUE KEY unique_buchung (datum, raum)
-);
+DROP TABLE IF EXISTS `buchungen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buchungen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `raum` varchar(50) NOT NULL,
+  `start_datum` date NOT NULL,
+  `end_datum` date NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
